be6506e9c3a01a98c673497c41523a6c601e7e05
PandeJanuartha <pandejanuarthabagus123@gmail.com>
2026-06-14 19:52:08 +0800

    Implementasi pencarian berbasis Discriminator Type Mapping dan fix privacy leak data kapasitas

--- src/repository/EventRepository.java
+++ src/repository/EventRepository.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Map;
 
 /**
- * Repositori data access untuk persistensi entitas Event menggunakan JDBC standar.
+ * repositori data access untuk persistensi entitas Event menggunakan JDBC standar.
  */
 public class EventRepository {
 
