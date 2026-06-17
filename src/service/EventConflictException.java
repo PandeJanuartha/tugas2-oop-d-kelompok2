ac229b5a99cc26e51a0ef9c59631acf0eafc9f21
PandeJanuartha <pandejanuarthabagus123@gmail.com>
2026-06-17 21:54:28 +0800

    Buat custom exception EventConflictException dan service utama untuk validasi jadwal venue

---
+++ src/service/EventConflictException.java
@@ -0,0 +1,15 @@
+package service;
+
+/**
+ * Custom Exception pendukung pelaporan kegagalan bentrok lokasi pengerjaan event.
+ */
+public class EventConflictException extends Exception {
+
+    public EventConflictException() {
+        super("Terjadi konflik penjadwalan pada data Event.");
+    }
+
+    public EventConflictException(String message) {
+        super(message);
+    }
+}
